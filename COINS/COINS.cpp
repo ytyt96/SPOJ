9b0cc447d8b7e5bf7a77ecb15285639acb111d53
Yuting Zhang <ytyt96@live.com>
2016-03-04 23:31:05 -0600

    New problem

--- COINS/COINS.cpp
+++ COINS/COINS.cpp
@@ -9,19 +9,19 @@ using namespace std;
 map<long long, long long> memo;
 
 long long dp(long long n){
-	long long& cell = memo[n];
-	if (cell > 0)
-		return cell;
-	if (n == 0)
-		return 0;
-	long long split = dp(n / 2) + dp(n / 3) + dp(n / 4);
-	cell = max(split, n);
-	return cell;
+    long long& cell = memo[n];
+    if (cell > 0)
+        return cell;
+    if (n == 0)
+        return 0;
+    long long split = dp(n / 2) + dp(n / 3) + dp(n / 4);
+    cell = max(split, n);
+    return cell;
 }
 
 int main(){
-	long long n = 0;
-	while (scanf("%lld", &n) == 1){
-		printf("%lld\n", dp(n));
-	}
+    long long n = 0;
+    while (scanf("%lld", &n) == 1){
+        printf("%lld\n", dp(n));
+    }
 }
